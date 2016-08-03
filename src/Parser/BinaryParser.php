a443b9de39ae474c2d6fcbffad8d10dd76e93db2
John Smith <john@graze.com>
2016-08-03 16:47:24 +0100

    Binary Parser

---
+++ src/Parser/BinaryParser.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of graze/unicontroller-client.
+ *
+ * Copyright (c) 2016 Nature Delivered Ltd. <https://www.graze.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license https://github.com/graze/unicontroller-client/blob/master/LICENSE.md
+ * @link https://github.com/graze/unicontroller-client
+ */
+namespace Graze\UnicontrollerClient\Parser;
+
+use Graze\UnicontrollerClient\Parser\Parser\ParserInterface;
+
+class BinaryParser
+{
+    /**
+     * @param string $string
+     * @return string
+     */
+    public function parse($string)
+    {
+        list(, $length, $binaryData) = explode(',', $string, 3);
+        $binaryData = substr($binaryData, 0, -10); // remove 'BinaryEnd,'
+        return trim($binaryData, "\r\n");
+    }
+}
